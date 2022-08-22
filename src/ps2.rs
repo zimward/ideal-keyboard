acbf73c174ba67951d61716f303ac999693be843
zimward <96021122+zimward@users.noreply.github.com>
2022-08-22 16:21:51 +0200

    Added PS2 Host to Device communication

--- src/ps2.rs
+++ src/ps2.rs
@@ -3,18 +3,18 @@ use core::{convert::Infallible, usize};
 use bitvec::order::Lsb0;
 use bitvec::prelude::*;
 use embedded_hal::digital::v2::{InputPin, StatefulOutputPin};
-use ringbuffer::{ConstGenericRingBuffer, RingBuffer, RingBufferRead};
-use riscv::{
-    asm::delay,
-    interrupt::{disable, enable},
-};
+use ringbuffer::{ConstGenericRingBuffer, RingBuffer, RingBufferRead, RingBufferWrite};
 
 enum OperatingMode {
     Idle,
     Transmit,
     SendParity,
     SendStop,
+    ReceiveStart,
     Receive,
+    ReceiveParity,
+    SendAck,
+    SendNack,
 }
 pub struct PS2<DATA, CLOCK>
 where
@@ -23,7 +23,8 @@ where
 {
     data: DATA,
     clock: CLOCK,
-    send_buffer: BitArr!(for 64,in usize,Lsb0),
+    send_buffer: BitArr!(for 8,in u8,Lsb0),
+    receive_buffer: BitArr!(for 8,in u8,Lsb0),
     current_bit: usize,
     operation: OperatingMode,
 }
@@ -36,7 +37,8 @@ where
         Self {
             data,
             clock,
-            send_buffer: bitarr!(usize,Lsb0;0;64),
+            send_buffer: bitarr!(u8,Lsb0;0;8),
+            receive_buffer: bitarr!(u8,Lsb0;0,8),
             current_bit: 0,
             operation: OperatingMode::Idle,
         }
@@ -44,21 +46,18 @@ where
     pub fn update<const LEN: usize>(
         &mut self,
         transmit_buffer: &mut ConstGenericRingBuffer<u8, { LEN }>,
+        receive_buffer: &mut ConstGenericRingBuffer<u8, { LEN }>,
     ) {
         //const_assert_ne!(transmit_buffer.len(), 0);
         //reset clock and check for host transmission
         if self.clock.is_set_low().unwrap() {
             let _ = self.clock.set_high();
-            unsafe {
-                disable();
-                delay(100); //wait ~ 1µS for clock to go high
-                enable();
-            }
-            if self.clock.is_low().unwrap() {
-                //Host is going to transmit
-                self.operation = OperatingMode::Receive;
-            }
             return; //wait till next clock
+        }
+        if self.clock.is_low().unwrap() {
+            //Host is going to transmit
+            let _ = self.data.set_high(); //release data
+            self.operation = OperatingMode::ReceiveStart;
         }
         match &self.operation {
             OperatingMode::Idle => {
@@ -92,36 +91,53 @@ where
             OperatingMode::SendStop => {
                 let _ = self.data.set_high();
                 let _ = self.clock.set_low();
-                if self
-                    .send_buffer
-                    .get(self.current_bit..64)
-                    .unwrap()
-                    .not_any()
-                    || self.current_bit == 64
-                {
+                if self.current_bit == 8 {
                     self.operation = OperatingMode::Idle;
                     self.current_bit = 0;
                 }
             }
-            OperatingMode::Receive => {}
+            OperatingMode::ReceiveStart => {
+                if self.data.is_low().unwrap() {
+                    if self.clock.is_high().unwrap() {
+                        self.operation = OperatingMode::Receive;
+                        let _ = self.clock.set_low();
+                        self.current_bit = 0;
+                    }
+                } else {
+                    self.operation = OperatingMode::Idle;
+                }
+            }
+            OperatingMode::Receive => {
+                self.receive_bit();
+            }
+            OperatingMode::ReceiveParity => {
+                let parity = self.data.is_high().unwrap();
+                let expected_parity = self.receive_buffer.count_ones() % 2 == 0;
+                if parity == expected_parity {
+                    self.operation = OperatingMode::SendAck;
+                    transmit_buffer.push(self.receive_buffer.as_raw_slice()[0]);
+                } else {
+                    self.operation = OperatingMode::SendNack;
+                }
+                let _ = self.clock.set_low();
+            }
+            OperatingMode::SendAck => {
+                self.send_ack(true);
+            }
+            OperatingMode::SendNack => {
+                self.send_ack(false);
+            }
         }
     }
     fn load_scancodes<const LEN: usize>(
         &mut self,
         transmit_buffer: &mut ConstGenericRingBuffer<u8, { LEN }>,
     ) -> bool {
-        /*        let mut new_codes = false;
-        for i in 0..8 {
-            if transmit_buffer.is_empty() {
-                break;
-            }
-            new_codes = true;
-            let section = self.send_buffer.get_mut(8 * i..8 * (i + 1)).unwrap();
-            let next = transmit_buffer.dequeue().unwrap();
-            section.store(next);
+        /*
+        if transmit_buffer.is_empty() {
+            return false;
         }
-        new_codes
-        */
+        self.send_buffer.store(transmit_buffer.dequeue().unwrap());*/
         self.send_buffer.get_mut(0..8).unwrap().store(0b1010_1010);
         true
     }
@@ -138,5 +154,27 @@ where
         }
         let _ = self.clock.set_low();
     }
-    fn receive_bit() {}
+    fn receive_bit(&mut self) {
+        if self.data.is_high().unwrap() {
+            self.receive_buffer.set(self.current_bit, true);
+        } else {
+            self.receive_buffer.set(self.current_bit, false);
+        }
+        let _ = self.clock.set_low();
+        self.current_bit += 1;
+        if self.current_bit == 8 {
+            self.operation = OperatingMode::ReceiveParity;
+        }
+    }
+    fn send_ack(&mut self, ack: bool) {
+        if ack {
+            let _ = self.data.set_low();
+            self.operation = OperatingMode::Idle;
+        } else {
+            let _ = self.data.set_high();
+            self.current_bit = 0;
+            self.operation = OperatingMode::Receive;
+        }
+        let _ = self.clock.set_low();
+    }
 }
