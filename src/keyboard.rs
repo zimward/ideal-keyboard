e19d97bb6c2c26a7ecc18a3ad3f96531e37859cd
zimward <96021122+zimward@users.noreply.github.com>
2022-08-23 23:34:17 +0200

    Added key handling logic to Keyboard struct + added delay to discharge diode capacitance

--- src/keyboard.rs
+++ src/keyboard.rs
@@ -1,29 +1,68 @@
+use crate::ps2::PS2;
+use crate::{keyboard_layouts::SCANCODE_LOOKUP, sprintln};
 use bitvec::prelude::*;
-use embedded_hal::digital::v2::{InputPin, OutputPin};
+use core::convert::Infallible;
+use embedded_hal::digital::v2::{InputPin, OutputPin, StatefulOutputPin};
+use ringbuffer::{ConstGenericRingBuffer, RingBufferWrite};
+use riscv::asm::delay;
 
-pub struct Keyboard<M, const MC: usize>
+pub struct Keyboard<M, const MC: usize, Ps2Data, Ps2Clock>
 where
     M: ScanableMatrix,
+    Ps2Data: InputPin<Error = Infallible> + StatefulOutputPin<Error = Infallible>,
+    Ps2Clock: InputPin<Error = Infallible> + StatefulOutputPin<Error = Infallible>,
 {
     matricies: [M; MC],
+    ps2_interface: PS2<Ps2Data, Ps2Clock>,
     key_buffer: BitArr!(for 192),
+    scancode_buffer: ConstGenericRingBuffer<u8, 32>,
+    command_buffer: ConstGenericRingBuffer<u8, 32>,
 }
 
-impl<M, const MC: usize> Keyboard<M, MC>
+impl<M, const MC: usize, Ps2Data, Ps2Clock> Keyboard<M, MC, Ps2Data, Ps2Clock>
 where
     M: ScanableMatrix,
+    Ps2Data: InputPin<Error = Infallible> + StatefulOutputPin<Error = Infallible>,
+    Ps2Clock: InputPin<Error = Infallible> + StatefulOutputPin<Error = Infallible>,
 {
-    pub fn new(matricies: [M; MC]) -> Self {
+    pub fn new(matricies: [M; MC], ps2_data: Ps2Data, ps2_clock: Ps2Clock) -> Self {
         Self {
             matricies,
-            key_buffer: bitarr!(usize,Lsb0;0;192), //change to generics once const generics are
-                                                   //stable
+            key_buffer: bitarr!(usize,Lsb0;0;192),
+            scancode_buffer: ConstGenericRingBuffer::new(),
+            command_buffer: ConstGenericRingBuffer::new(),
+            ps2_interface: PS2::new(ps2_data, ps2_clock),
         }
     }
     pub fn scan(&mut self) {
         for matrix in &mut self.matricies {
             matrix.scan(&mut self.key_buffer);
         }
+    }
+    pub fn process_keystrokes(&mut self) {
+        for i in (0..self.key_buffer.len()).step_by(2) {
+            let val = self.key_buffer.get_mut(i..=i + 1).unwrap();
+            // read change bit
+            if *val.get(1).unwrap() {
+                let key = i / 2;
+                if key >= SCANCODE_LOOKUP.len() {
+                    return;
+                }
+                if *val.get(0).unwrap() {
+                    self.scancode_buffer.push(SCANCODE_LOOKUP[key][0]);
+                    sprintln!("Key {} pressed", key);
+                } else {
+                    self.scancode_buffer.push(0xF0); //break code
+                    self.scancode_buffer.push(SCANCODE_LOOKUP[key][0]);
+                    sprintln!("Key {} released", key);
+                }
+                val.set(1, false);
+            }
+        }
+    }
+    pub fn update_interface(&mut self) {
+        self.ps2_interface
+            .update(&mut self.scancode_buffer, &mut self.command_buffer);
     }
 }
 
@@ -86,25 +125,27 @@ where
                 match dat.is_high() {
                     Err(_) => {}
                     Ok(r) => {
+                        let key = (self.offset as usize + i * address_len + j) * 2;
+                        let bits = keybuffer.get_mut(key..=(key + 1)).unwrap();
                         if r {
-                            let key = (self.offset as usize + i * address_len + j) * 2;
-                            let bits = keybuffer.get_mut(key..=(key + 1)).unwrap();
-                            if bits.not_any() {
+                            //key wasn't already pressed
+                            if !*bits.get(0).unwrap() {
                                 bits.set(0, true); //key bit
                                 bits.set(1, true); //change bit
                             }
-                        } else {
-                            let key = (self.offset as usize + i * address_len + j) * 2;
-                            let bits = keybuffer.get_mut(key..=(key + 1)).unwrap();
-                            if bits.any() {
-                                bits.set(0, false);
-                                bits.set(1, true);
-                            }
+                        } else if *bits.get(0).unwrap() {
+                            //key was pressed before
+                            bits.set(0, false);
+                            bits.set(1, true);
                         }
                     }
                 }
             }
             let _ = addr.set_low();
+            unsafe {
+                delay(100); //wait for diode capacitance to discharge ~1µS should be sufficient, if
+                            //not a keypress will register as the current and following key.
+            }
         }
     }
 }
